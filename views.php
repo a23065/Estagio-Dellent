638e19ae55d6c9533d6d430e1979a4e2351b4332
a23065 <a23065@ua.pt>
2016-06-11 22:39:03 +0100

    Interação com Equipa recrutamento
    corercção de indentação

--- views.php
+++ views.php
@@ -22,61 +22,61 @@ $msg = "Criadas views em ".$app_name." : \n";
 //Criar uma view + de 48h para avaliação
 //
 PodioView::create( APP_ID, $attributes = array(
-					'name' => '+48h para avaliação',
-					'private' => false,										//vista pública
-					'filters' => array(
-								'applicant-stage' 	=> array(null, 14),		//sem applicant stage definido (por avaliar)
-								'created_on'		=> array (				//data de criação
-														'to' => '+0dr',		//sem data de ínicio mas com data de fim (2 dias antes)
-														'from' => '-2dr',	//sem data de ínicio mas com data de fim (2 dias antes)
-														)
-								),
-					"grouping" => array (									//corresponde ao Split no interface
-								"type" => "field",
-								"sub_value" => null,
-								"value"=> 119290949 ) 						//Total shocolarship (119290949)							
-								));
+	'name' => '+48h para avaliação',
+	'private' => false,										//vista pública
+	'filters' => array(
+				'applicant-stage' 	=> array(null, 14),		//sem applicant stage definido (por avaliar)
+				'created_on'		=> array (				//data de criação
+										'to' => '+0dr',		//sem data de ínicio mas com data de fim (2 dias antes)
+										'from' => '-2dr',	//sem data de ínicio mas com data de fim (2 dias antes)
+										)
+				),
+	"grouping" => array (									//corresponde ao Split no interface
+				"type" => "field",
+				"sub_value" => null,
+				"value"=> 119290949 ) 						//Total shocolarship (119290949)							
+				));
 
 $msg .= " + de 48h para avaliação [".date('Y-m-d H:i:s')."]\n";
 
 //Criar uma view + de 48h para ser contactado
 //
 PodioView::create( APP_ID, $attributes = array(
-					'name' => '+48h para contacto',
-					'private' => false,										//vista pública
-					'filters' => array(
-								'applicant-stage' 	=> 6,					//option 6: To be contacted
-								'last_edit_on'		=> array (				//data de criação
-														'to' => '+0dr',		//sem data de ínicio mas com data de fim (2 dias antes)
-														'from' => '-2dr',	//sem data de ínicio mas com data de fim (2 dias antes)
-														)
-								),
-					"grouping" => array (									//corresponde ao Split no interface
-								"type" 		=> "field",
-								"sub_value" => null,
-								"value"		=> 119290955 ) 					//main-activity (119290955)							
-								));
-								
+	'name' => '+48h para contacto',
+	'private' => false,										//vista pública
+	'filters' => array(
+				'applicant-stage' 	=> 6,					//option 6: To be contacted
+				'last_edit_on'		=> array (				//data de criação
+										'to' => '+0dr',		//sem data de ínicio mas com data de fim (2 dias antes)
+										'from' => '-2dr',	//sem data de ínicio mas com data de fim (2 dias antes)
+										)
+				),
+	"grouping" => array (									//corresponde ao Split no interface
+				"type" 		=> "field",
+				"sub_value" => null,
+				"value"		=> 119290955 ) 					//main-activity (119290955)							
+				));
+				
 
 $msg .= " + de 48h para ser contactado [".date('Y-m-d H:i:s')."]\n";	
 							
 //Criar uma view + de 5 dias em Interview
 //
 PodioView::create( APP_ID, $attributes = array(
-					'name' => '+5 dias em entrevista',
-					'private' => false,										//vista pública
-					'filters' => array(
-								'applicant-stage' 	=> 7,					//option 7: Interviewing
-								'last_edit_on'		=> array (				//data de edição
-														'to' => '+0dr',		//sem data de ínicio mas com data de fim (2 dias antes)
-														'from' => '-5dr',	//sem data de ínicio mas com data de fim (2 dias antes)
-														)
-								),
-					"grouping" => array (									//corresponde ao Split no interface
-								"type" 		=> "field",
-								"sub_value" => null,
-								"value"		=> 119290956 ) 					//Expertise areas (119290956)								
-								));
+		'name' => '+5 dias em entrevista',
+		'private' => false,										//vista pública
+		'filters' => array(
+					'applicant-stage' 	=> 7,					//option 7: Interviewing
+					'last_edit_on'		=> array (				//data de edição
+											'to' => '+0dr',		//sem data de ínicio mas com data de fim (2 dias antes)
+											'from' => '-5dr',	//sem data de ínicio mas com data de fim (2 dias antes)
+											)
+					),
+		"grouping" => array (									//corresponde ao Split no interface
+					"type" 		=> "field",
+					"sub_value" => null,
+					"value"		=> 119290956 ) 					//Expertise areas (119290956)								
+					));
 
 $msg .= " + de 5 dias em Interviewing [".date('Y-m-d H:i:s')."]\n";	
 							
